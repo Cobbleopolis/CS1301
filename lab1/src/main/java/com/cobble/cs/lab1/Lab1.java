1ae8fd08c9d6eac6478d2b2156e9010c3505ec23
Cobbleopolis <cobbleopolis@gmail.com>
2016-08-19 11:27:30 -0400

    Added comments and began on Exercise 6

--- lab1/src/main/java/com/cobble/cs/lab1/Lab1.java
+++ lab1/src/main/java/com/cobble/cs/lab1/Lab1.java
@@ -20,7 +20,9 @@ public class Lab1 {
         System.out.println("Lab 1 | Exercise 5");
         exercise5();
         System.out.println(separator);
-        //TODO Exercise 6
+        System.out.println("Lab 1 | Exercise 6");
+        exercise6();
+        System.out.println(separator);
     }
 
     private static void exercise1() {
@@ -40,14 +42,14 @@ public class Lab1 {
     }
 
     private static void exercise3() {
-        System.out.println("1*2*3*4*5: " + (2 * 3 *4 * 5));
+        System.out.println("1*2*3*4*5: " + (2 * 3 * 4 * 5)); //1 isn't needed in the multiplication
     }
 
     private static void exercise4() {
-        int width = 3;
-        int height = 5;
-        System.out.println("Area of a " + width + "x" + height+ ": " + (width * height));
-        System.out.println("Parameter of a " + width + "x" + height+ ": " + ((width * 2) + (height * 2)));
+        int width = 3; //Variable to hold the width of a rectangle
+        int height = 5; //Variable to hold the height of a rectangle
+        System.out.println("Area of a " + width + "x" + height+ ": " + (width * height)); //Prints and calculates the area of a rectangle using the `width` and `height` variables
+        System.out.println("Parameter of a " + width + "x" + height+ ": " + ((width * 2) + (height * 2))); //Prints and calculates the parameter of a rectangle using the `width` and `height` variables
     }
 
     private static void exercise5() {
@@ -55,6 +57,17 @@ public class Lab1 {
         speed *= 60f; //Miles per hour
         speed *= 1.6f; //Km per hour
         System.out.println("Average speed in Km/h: " + speed);
+    }
 
+    private static void exercise6() {
+        //Prints the info about the errors in Listing 1.4
+        System.out.println("Listing 1.4:\n" +
+                "\tThere are really only 2 errors in this file.\n" +
+                "\t\tFirst, the main method doesn't have it's required return type of \"void\".\n" +
+                "\t\tSecond, there is no closing quote for the string that is passed to `System.out.println`.\n" +
+                "\t\tAs a side effect of the second syntax error the compiler says that there is no ending semi-colon on line 3.\n" +
+                "\t\tThe compiler also says that there is an unexpected end of file because the compiler is still looking for closing curly braces for the blocks associated with the main method and the class because the string on line 3 is never closed.");
+        //TODO Listing 1.5
+        //TODO Listing 1.6
     }
 }
