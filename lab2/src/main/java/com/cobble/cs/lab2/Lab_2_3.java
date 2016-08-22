50bd12e21907a65913504610463b00af729ed187
Cobbleopolis <cobbleopolis@gmail.com>
2016-08-22 13:37:38 -0400

    Worked on Lab2

---
+++ lab2/src/main/java/com/cobble/cs/lab2/Lab_2_3.java
@@ -0,0 +1,36 @@
+//Class:      CS1301
+//Term:       2016
+//Name:       Logan Thompson
+//Instructor: Monisha Verma
+//Assignment: Lab 2_3
+
+package com.cobble.cs.lab2;
+
+import java.util.Scanner;
+
+public class Lab_2_3 {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Input byte value: "); //Gets the byte value
+        byte byte_value = scanner.nextByte(); //Waits for a byte value
+
+        System.out.print("Input short value: "); //Gets the short value
+        short short_value = scanner.nextByte(); //Waits for a short value
+
+        System.out.print("Input int value: "); //Gets the int value
+        int int_value = scanner.nextInt(); //Waits for a int value
+
+        System.out.print("Input long value: "); //Gets the long value
+        long long_value = scanner.nextInt(); //Waits for a long value
+
+        System.out.print("Input float value: "); //Gets the float value
+        float float_value = scanner.nextInt(); //Waits for a float value
+
+        System.out.print("Input double value: "); //Gets the double value
+        double double_value = scanner.nextDouble(); //Waits for a double value
+
+        //TODO printing values
+    }
+}
