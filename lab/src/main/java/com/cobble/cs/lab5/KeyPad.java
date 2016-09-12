9c9add18d1c570640b7c852c7122bd370ec952b4
Cobbleopolis <cobbleopolis@gmail.com>
2016-09-12 16:22:28 -0400

    Commented Lab 5.4

--- lab/src/main/java/com/cobble/cs/lab5/KeyPad.java
+++ lab/src/main/java/com/cobble/cs/lab5/KeyPad.java
@@ -15,28 +15,30 @@ public class KeyPad {
     public static void main(String[] args) {
         System.out.print("Enter a letter: ");
 
+        /** Stores the forced lowercase char inputted by the user. */
         char c = scanner.next().toLowerCase().charAt(0);
 
+        /** Stores what key the char is on with a default invalid state of -1. */
         int key = -1;
 
-        if (c >= 'a' && c <= 'c')
+        if (c >= 'a' && c <= 'c') // Checks if on key 2
             key = 2;
-        else if (c >= 'd' && c <= 'f')
+        else if (c >= 'd' && c <= 'f') // Checks if on key 3
             key = 3;
-        else if (c >= 'g' && c <= 'i')
+        else if (c >= 'g' && c <= 'i') // Checks if on key 4
             key = 4;
-        else if (c >= 'j' && c <= 'l')
+        else if (c >= 'j' && c <= 'l') // Checks if on key 5
             key = 5;
-        else if (c >= 'm' && c <= 'o')
+        else if (c >= 'm' && c <= 'o') // Checks if on key 6
             key = 6;
-        else if (c >= 'p' && c <= 's')
+        else if (c >= 'p' && c <= 's') // Checks if on key 7
             key = 7;
-        else if (c >= 't' && c <= 'v')
+        else if (c >= 't' && c <= 'v') // Checks if on key 8
             key = 8;
-        else if (c >= 'w' && c <= 'z')
+        else if (c >= 'w' && c <= 'z') // Checks if on key 9
             key = 9;
 
-        if (key == -1)
+        if (key == -1) // Detects if key is still in its invalid state.
             System.out.println("Invalid option '" + c + "'.");
         else
             System.out.println("The corresponding number is " + key);
