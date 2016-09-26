f8ed82897e8744d9f6d474ea7cf25daf8bb909c5
Cobbleopolis <cobbleopolis@gmail.com>
2016-09-26 15:45:42 -0400

    Using replaceAll in all cases

--- gp/src/main/java/com/cobble/cs/gp1/GroupProject1.java
+++ gp/src/main/java/com/cobble/cs/gp1/GroupProject1.java
@@ -8,20 +8,16 @@ package com.cobble.cs.gp1;
 
 public class GroupProject1 {
 
-    private static final String printStr = "#\n##\n###\n##\n#";
+    private static final String printStr = "\0\n\0\0\n\0\0\0\n\0\0\n\0";
 
-    private static final String[] replaceStrs = {"+", "^", "~", "\\$"};
+    private static final String[] replaceStrs = {"#", "+", "^", "~", "\\$"};
 
     public static void main(String[] args) {
 
         //FRAGMENT GET REST OF CODE
 
         /** Stores what group the char was in */
-        for (int group = 1; group <= 5; group++) {
-            if (group == 1)
-                System.out.println(printStr);
-            else
-                System.out.println(printStr.replaceAll("#", replaceStrs[group - 2]));
-        }
+        for (int group = 1; group <= 5; group++)
+            System.out.println(printStr.replaceAll("\0", replaceStrs[group - 1]));
     }
 }
