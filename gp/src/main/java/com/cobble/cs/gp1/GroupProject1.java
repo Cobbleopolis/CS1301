e7d1bfef08015c2d94ebb06bb839123d1ad71326
Cobbleopolis <cobbleopolis@gmail.com>
2016-10-03 13:45:05 -0400

    Fixed commenting in GP1

--- gp/src/main/java/com/cobble/cs/gp1/GroupProject1.java
+++ gp/src/main/java/com/cobble/cs/gp1/GroupProject1.java
@@ -4,6 +4,38 @@
 //Instructor: Monisha Verma
 //Assignment: GroupProject1
 
+/**
+ * Description:
+ * 1.Generate a number between 97 & 122 ( inclusive )
+ * 2. Convert the number to a character (ASCII)
+ * 3. Place the character  into 1 of 5 groups
+ * --
+ * a-f : 1
+ * g-l : 2
+ * m-r: 3
+ * s-x: 4
+ * y-z: 5
+ *
+ * 4. If  in group 1
+ * print out picture with "#" using loop
+ *
+ * #
+ * ##
+ * ###
+ * ##
+ * #
+ *
+ * If in group 2 print picture with "t" using loop
+ * if in group 3 print picture with "^" using loop
+ * If in group 4 print picture with "~"
+ * if in group 5 print picture with "$"
+ *
+ * Assumptions: All character values are between 97 and 122
+ * Rules: If character is in group 1 the image will be made in "#", if in group 2 the image will be made in "+", if in group 3 the image will be made in "^", if in group 4 the image will be made in "~", if in group 5 the image will be made in "$"
+ * Inputs: None from user.
+ * Outputs: The character, the character's group, and the image based on the character's group
+ */
+
 package com.cobble.cs.gp1;
 
 import java.util.Random;
