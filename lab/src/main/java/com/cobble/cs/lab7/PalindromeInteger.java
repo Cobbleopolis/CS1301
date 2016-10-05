9e230861ab9be8ccc7d3903a51b1139b2de9a43b
Cobbleopolis <cobbleopolis@gmail.com>
2016-10-05 15:50:09 -0400

    Added re-run to Lab 7.3

--- lab/src/main/java/com/cobble/cs/lab7/PalindromeInteger.java
+++ lab/src/main/java/com/cobble/cs/lab7/PalindromeInteger.java
@@ -14,22 +14,31 @@ public class PalindromeInteger {
 
     public static void main(String[] args) {
 
-        System.out.print("Enter a number: "); // Prompts the user for the number
+        /** Stores if the user wants to re-run the program. */
+        boolean reRun;
 
-        /** Stores the inputted int from the user. */
-        int n = scanner.nextInt();
+        do {
+            System.out.print("Enter a number: "); // Prompts the user for the number
 
-        System.out.printf("%d is %sa palindrome.", n, isPalindrome(n) ? "" : "not "); // Prints if n is a palindrome.
+            /** Stores the inputted int from the user. */
+            int n = scanner.nextInt();
+
+            System.out.printf("%d is %sa palindrome.%n", n, isPalindrome(n) ? "" : "not "); // Prints if n is a palindrome.
+
+            System.out.print("Do you want to re-run the program? [y/n]: "); // Prompts the user to re-run the program
+            reRun = scanner.next().toLowerCase().charAt(0) == 'y'; // Checks the user input to re-run the program
+        } while (reRun);
     }
 
     /**
      * Reverses the digits of the passed integer.
+     *
      * @param n The integer to reverse the digits of.
      * @return {@code n} with its digits reversed.
      */
     private static int reverse(int n) {
         int reversedInt = 0;
-        while(n > 0) {
+        while (n > 0) {
             reversedInt = reversedInt * 10 + n % 10;
             n /= 10;
         }
@@ -38,6 +47,7 @@ public class PalindromeInteger {
 
     /**
      * Calculates if the passed int is a palindrome.
+     *
      * @param n The integer to check if it is a palindrome.
      * @return {@code true} if n is a palindrome, {@code false} otherwise.
      */
