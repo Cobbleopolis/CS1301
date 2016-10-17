a48a5e54430e98f7e59e655a0f8278b389729285
Cobbleopolis <cobbleopolis@gmail.com>
2016-10-17 14:31:54 -0400

    Did lab 9.1

---
+++ lab/src/main/java/com/cobble/cs/lab9/AssigningGrades.java
@@ -0,0 +1,44 @@
+//Class:      CS1301
+//Term:       2016
+//Name:       Logan Thompson
+//Instructor: Monisha Verma
+//Assignment: AssigningGrades
+
+package com.cobble.cs.lab9;
+
+import java.util.Scanner;
+
+public class AssigningGrades {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Enter the number of students: "); // Prompts the user to enter number of students.
+        /** Stores the grades. */
+        int[] grades = new int[scanner.nextInt()];
+
+        System.out.print("Enter " + grades.length + " scores: ");
+        for (int i = 0; i < grades.length; i++)
+            grades[i] = scanner.nextInt();
+
+        /** Stores the best score. */
+        int bestScore = grades[0];
+
+        for (int i : grades)
+            if (i > bestScore)
+                bestScore = i;
+
+        for (int i = 0; i < grades.length; i++) {
+            String grade = "F";
+            if (grades[i] >= bestScore - 10)
+                grade = "A";
+            else if (grades[i] >= bestScore - 20)
+                grade = "B";
+            else if (grades[i] >= bestScore - 30)
+                grade = "C";
+            else if (grades[i] >= bestScore - 40)
+                grade = "D";
+            System.out.printf("Student %d score is %d and grade %s%n", i, grades[i], grade);
+        }
+    }
+}
